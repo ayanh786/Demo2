de553d338b1b721604795f707277980e6bac9b11
Abdul <abmalik1229@gmail.com>
2022-03-11 12:04:43 -0500

    created printQuotient Method

--- nice.java
+++ nice.java
@@ -4,6 +4,14 @@ public class nice{
     public nice(){
 
     }
+
+    public void printQuotient(float x, float y){
+
+        float remainder = x/y; 
+        System.out.printf(remainder, 3)
+
+    }
+
     public static void main(String args[]){
         nice n = new nice();
     }
