5c86f7cc2c649409f0f4a56b56b58e9829c78eb7
ayanh786 <ayanh7879@gmail.com>
2022-03-15 12:34:24 -0400

    perfect square done

--- nice.java
+++ nice.java
@@ -2,13 +2,19 @@
 
 public class nice{
 
+	public nice(){
+		System.out.println(getPerfectSquare().ps(169));
+    }
+
 	interface PerfectSquare{
 		boolean ps(int num);
 	}
 
-    public nice(){
+	public static PerfectSquare getPerfectSquare(){
 
-    }
+		return (num) -> Math.sqrt(num)-Math.floor(Math.sqrt(num))==0;
+	}
+
     public static void main(String args[]){
         nice n = new nice();
     }
